e5a647a935986c211b9a2191270577ea7924b33f
Jefferson V. Sarmiento <jvsarmiento.webdev@gmail.com>
2024-04-02 17:29:58 +0800

    Modified create_blogs_table migration

--- database/migrations/2024_04_02_091711_create_blogs_table.php
+++ database/migrations/2024_04_02_091711_create_blogs_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
