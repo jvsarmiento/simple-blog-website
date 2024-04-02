7ac3123f46b89fc4774cb799b7b8b254f73f2016
Jefferson V. Sarmiento <jvsarmiento.webdev@gmail.com>
2024-04-02 19:57:23 +0800

    Defined relationship of Comment Model to Blog Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
@@ -14,4 +15,9 @@ class Comment extends Model
         'comment',
         'user_id'
     ];
+
+    public function blog(): BelongsTo
+    {
+        return $this->belongsTo(Blog::class);
+    }
 }
